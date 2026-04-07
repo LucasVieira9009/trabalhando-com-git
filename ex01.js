120af7d7e60c49751d3f07827866a15f83968329
LucasVieira9009 <lvs.lucasvieira2009@email.com>
2026-04-07 15:10:01 -0300

    feat:exercicio 1 finalizado

---
+++ ex01.js
@@ -0,0 +1,38 @@
+// const readline = require('readline-sync')
+
+// let nome = readline.question("Digite o seu nome: ")
+
+// // console.log(`Seu nome é: ${nome}`)
+
+
+// Importa a biblioteca readline-sync
+// const readline = require("readline-sync")
+
+// // Solicita os dados dos usuarios
+// let nomeDoAluno = readline.question("Digite o seu nome: ")
+// let curso = readline.question("Informe o curso: ") 
+// let idade = readline.questionInt("Informe a sua idade: ")
+
+// console.log("====== SISTEMA DE CADASTRO ======")
+// console.log(`Nome do usuário: ${nomeDoAluno}`)
+// console.log(`Curso do usuário: ${curso}`)
+// console.log(`Idade do usuário: ${idade}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
